1ef2c7557ab3ff2a6048ca2523f279c352198f9a
arman93061xxxxx <as5132128@gmail.com>
2026-02-11 11:17:37 +0530

    done

---
+++ bus_sure/utils/logger.ts
@@ -0,0 +1,11 @@
+
+// logger.ts
+// Simple logger utility for learning purposes
+
+export function logInfo(message: string) {
+  console.log("[INFO]", message);
+}
+
+export function logError(message: string) {
+  console.log("[ERROR]", message);
+}
